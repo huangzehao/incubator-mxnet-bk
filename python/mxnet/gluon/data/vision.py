8c26fbc06e0b7c389bdb728750d3c08c029e3332
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-19 02:00:25 +0000

    Adding tarfile member sanitization to extractall()

--- python/mxnet/gluon/data/vision.py
+++ python/mxnet/gluon/data/vision.py
@@ -110,7 +110,26 @@ class CIFAR10(_DownloadedDataset):
         filename = download(url, self._root)
 
         with tarfile.open(filename) as tar:
-            tar.extractall(self._root)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(tar, self._root)
 
         if self._train:
             filename = os.path.join(self._root, 'cifar-10-batches-bin/data_batch_%d.bin')
